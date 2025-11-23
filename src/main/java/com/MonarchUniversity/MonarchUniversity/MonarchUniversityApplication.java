3ed874ccccf165d774cd755cf11d3914b9afcc84
shalom-geek-ng <oluwafemishalom3@gmail.com>
2025-11-23 23:38:37 +0100

    update branch

--- src/main/java/com/MonarchUniversity/MonarchUniversity/MonarchUniversityApplication.java
+++ src/main/java/com/MonarchUniversity/MonarchUniversity/MonarchUniversityApplication.java
@@ -1,9 +1,16 @@
 package com.MonarchUniversity.MonarchUniversity;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MetricsAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+	    exclude = {
+	        MetricsAutoConfiguration.class,
+	        SystemMetricsAutoConfiguration.class
+	    }
+	)
 public class MonarchUniversityApplication {
 
 	public static void main(String[] args) {
